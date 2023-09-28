a01bd6b3f38f042f95be9a635afe6891477650c0
Benson Mwangi <kimaniben@gmail.com>
2023-09-28 08:10:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+//Multiplication
+let num1=31;
+let num2=2;
+let multiply= num1*num2;
+console.log(multiply);
+// Math.random
+let random = (1,100);
+function getRandomInt(min,max){
+    return Math.floor(Math.random() * (max-min+1)) + min;
+}
+let randomInt=getRandomInt(1,100);
+console.log(randomInt);
+// Remainder
+let num3 =22;
+let num4 =6;
+let mod = num3%num4;
+console.log (mod);
+//Max
+let max=20;
+console.log(Math.max(2,3,15,20));
